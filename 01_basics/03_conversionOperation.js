b0fa149a10bbc401fb732773b77566585d76be26
TarunPatidar04 <147477104+TarunPatidar04@users.noreply.github.com>
2023-10-10 11:34:17 +0000

    git commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,57 @@
+// let score =33;
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score ="33abc";
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let score =null;
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score =undefined;
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let score =true;
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33 
+// "33abc" =>NaN
+// true=>1; false=>0
+
+
+// let isLoggedIn=1; true
+// let isLoggedIn=0;
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn="";
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+
+// let isLoggedIn="Yary";
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
