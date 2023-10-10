f44bbe6457487fe84b490329d54f3481d10791d4
TarunPatidar04 <147477104+TarunPatidar04@users.noreply.github.com>
2023-10-10 11:12:56 +0000

    git commit1

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"Use Strict"; //treat all js code as newer version
+
+// alert("hello"); //we are using nodejs , not brower
+
+console.log(3 + 3);
+
+console.log("Tarun");
+
+// let name="Tarun";
+// let age =18;
+// let isLoggin=false;
+// let state =null;
+// undefined
+// symbol==>unique
+
+
+//object
+
+
+console.log(typeof "Tarun");
+console.log(typeof null);
