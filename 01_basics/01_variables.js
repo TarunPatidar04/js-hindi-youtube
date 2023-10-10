4e2b8361b472f0edaabe61a00d05c609b140efea
TarunPatidar04 <147477104+TarunPatidar04@users.noreply.github.com>
2023-10-10 10:49:32 +0000

    git commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144533;
+let accountEmail = "tarun@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+// accountId = 2 //not allowed
+
+accountEmail ="tarun@gmail.com";
+accountPassword=3213;
+accountCity="Benguluru";
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
